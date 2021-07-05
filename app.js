cb577d246fccee6e9c7ccf707a83576e85b0446e
Jahiz Habibullah <60274060+Jahiz446@users.noreply.github.com>
2021-07-05 10:17:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-jahiz:Habibjahiz1998@cluster0.wifdc.mongodb.net/blogDB", {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/todolistDB'", {useNewUrlParser: true});
 const postSchema = {
   title: String,
   content: String
